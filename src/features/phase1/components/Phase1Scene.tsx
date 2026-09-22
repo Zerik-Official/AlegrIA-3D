8ca700c3e6b38b7837a8704071dd5b803e129e40
Zerik <zerik@alegria.dev>
2026-09-22 08:17:11 -0500

    fix(phase1): restore useMemo import for arroyo shader

--- src/features/phase1/components/Phase1Scene.tsx
+++ src/features/phase1/components/Phase1Scene.tsx
@@ -1,4 +1,4 @@
-import { memo, useRef } from 'react'
+import { memo, useMemo, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 import { ModelLoader } from '../../../models/shared/ModelLoader'
