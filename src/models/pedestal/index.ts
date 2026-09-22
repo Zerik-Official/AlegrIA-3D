5fd65acf2564a51b06dc2627ce8328e26f188f5e
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-09-21 20:39:04 -0500

    feat(models): add pedestal domain re-exports

---
+++ src/models/pedestal/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Pedestal domain models.
+ * @module models/pedestal
+ * @link https://docs.blender.org/manual/en/latest/addons/import_export/scene_gltf2.html
+ */
+
+export { Pedestal as ProceduralPedestal } from '../../features/pedestal/components/Pedestal'
+export { LevitatingBook as ProceduralLevitatingBook } from '../../features/pedestal/components/LevitatingBook'
