1c2351f73224414f40678315eea16173376fc312
Zerik <zerik@alegria.dev>
2026-09-22 06:31:02 -0500

    refactor(pedestal): tune procedural pedestal material to warmer rustic tone

--- src/features/pedestal/components/Pedestal.tsx
+++ src/features/pedestal/components/Pedestal.tsx
@@ -31,7 +31,7 @@ function ProceduralPedestalGeometry() {
 
       <mesh position={[0, 0.65, 0]} castShadow receiveShadow>
         <cylinderGeometry args={[0.42, 0.52, 0.8, 32]} />
-        <meshStandardMaterial color="#2c1e0f" roughness={0.65} />
+        <meshStandardMaterial color="#3d2612" roughness={0.78} metalness={0.06} />
       </mesh>
       <mesh position={[0, 0.65, 0]}>
         <cylinderGeometry args={[0.44, 0.44, 0.78, 32, 1, true]} />
