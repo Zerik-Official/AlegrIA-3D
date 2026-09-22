1fdfc438d075172d4829398c0524ff37b5a4424c
Zerik <zerik@alegria.dev>
2026-09-22 07:49:22 -0500

    chore(phase1): remove unused useMemo import

--- src/features/phase1/components/Phase1Scene.tsx
+++ src/features/phase1/components/Phase1Scene.tsx
@@ -1,4 +1,4 @@
-import { memo, useMemo, useRef } from 'react'
+import { memo, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 import { ModelLoader } from '../../../models/shared/ModelLoader'
