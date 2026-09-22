81d72b43645c18c6c7969d68e8187d111873205d
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-09-21 20:41:17 -0500

    style(types): add JSDoc header to global augmentations

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,12 @@
+/**
+ * Global type augmentations for React Three Fiber.
+ * @module global
+ */
 import '@react-three/fiber'
 
 declare global {
   namespace JSX {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface IntrinsicElements {}
   }
 }
